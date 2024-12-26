8b09214b845a465ad59140f3a1316e172803a8bb
Praveena-relangi <relangipraveena99@gmail.com>
2024-12-26 15:03:38 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,8 @@
 # Uncomment the following imports before adding the Model code
-
 from django.db import models
 
-
 # Create your models here.
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -17,13 +16,14 @@ class CarModel(models.Model):
     CAR_TYPE_CHOICES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
-        ('WAGON', 'Wagon')
+        ('WAGON', 'Wagon'),
     ]
 
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=10, choices=CAR_TYPE_CHOICES,
-                            default='SUV')
+    type = models.CharField(
+        max_length=10, choices=CAR_TYPE_CHOICES, default='SUV'
+    )
     year = models.IntegerField(default=2023)
 
     def __str__(self):
