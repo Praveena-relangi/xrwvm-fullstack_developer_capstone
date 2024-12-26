3784b346e402b633f61987cc93987edca9c105e6
Praveena-relangi <relangipraveena99@gmail.com>
2024-12-26 14:57:46 +0530

    fixed pylint errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,33 +1,30 @@
-from django.db import models
-from django.utils.timezone import now
-from django.core.validators import MaxValueValidator, MinValueValidator
+# Uncomment the following imports before adding the Model code
 
+from django.db import models
+
+
+# Create your models here.
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # Other fields as needed
 
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-    name = models.CharField(max_length=100)
-    CAR_TYPES = [
+    CAR_TYPE_CHOICES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        # Add more choices as required
+        ('WAGON', 'Wagon')
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
-    # Other fields as needed
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=CAR_TYPE_CHOICES,
+                            default='SUV')
+    year = models.IntegerField(default=2023)
 
     def __str__(self):
         return self.name  # Return the name as the string representation
